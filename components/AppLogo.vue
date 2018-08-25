12902c0d9d70f5265cda5809552980476d02ee94
Adem ilter <ademilter@gmail.com>
2018-08-25 03:46:26 +0300

    dynamic image for lazy load

--- components/AppLogo.vue
+++ components/AppLogo.vue
@@ -65,16 +65,17 @@
       <br>
        <picture>
         <source media="(min-width: 1200px)"
-                data-srcset="http://a23.imgix.net/site-cover-photo.jpg?w=1.0&h=1300&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=60">
+                :data-srcset="require('~/assets/site-cover-photo-3.jpg')">
         <source media="(min-width: 992px)"
-                data-srcset="http://a23.imgix.net/site-cover-photo.jpg?w=1200&h=900&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=60">
+                :data-srcset="require('~/assets/site-cover-photo-2.jpg')">
         <source media="(min-width: 768px)"
-                data-srcset="http://a23.imgix.net/site-cover-photo.jpg?w=992&h=800&fit=crop&crop=focalpoint&fp-x=0.5&fp-y=0.3&auto=format&q=60">
+                :data-srcset="require('~/assets/site-cover-photo-1.jpg')">
         <img src="data:image/gif;base64,R0lGODlhAQABAAAAACH5BAEKAAEALAAAAAABAAEAAAICTAEAOw=="
-            data-src="http://a23.imgix.net/site-cover-photo.jpg?w=768&auto=format&q=60"
+            :data-src="require('~/assets/site-cover-photo-0.jpg')"
             class="img-fadein lazyload"
             alt="Adem ilter with daughter"
             data-expand="-30">
     </picture>
+    {{ a0 }}
   </div>
 </template>
