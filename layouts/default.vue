69bb82c00ee939dbd4b053ed4456a24f17b6e174
Adem ilter <ademilter@gmail.com>
2018-08-25 19:01:29 +0300

    html import

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,5 +1,19 @@
 <template>
   <div>
+    <Header/>
     <nuxt/>
+    <Footer/>
   </div>
 </template>
+
+<script>
+  import Header from './header'
+  import Footer from './footer'
+
+  export default {
+    components: {
+      Header,
+      Footer
+    }
+  }
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,33 +1,24 @@
 <template>
-  <section class="container">
-    <div>
-      <app-logo/>
-      <h1 class="title">
-        ademilter-nuxt
-      </h1>
-      <h2 class="subtitle">
-        Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
-      </div>
-    </div>
-  </section>
+  <main class="Main" role="main">
+    <About/>
+    <Cover/>
+    <Timeline/>
+    <Journal/>
+  </main>
 </template>
 
 <script>
-import AppLogo from '~/components/AppLogo.vue'
+  import About from './section-about'
+  import Cover from './section-cover'
+  import Timeline from './section-timeline'
+  import Journal from './section-journal'
 
-export default {
-  components: {
-    AppLogo
+  export default {
+    components: {
+      About,
+      Cover,
+      Timeline,
+      Journal
+    }
   }
-}
 </script>
