5c4fb6b25ec297c3f7c80bb3d3a150612f994059
Adem ilter <ademilter@gmail.com>
2020-04-23 23:09:19 +0300

    my-desk update

--- src/pages/my-desk.js
+++ src/pages/my-desk.js
@@ -80,7 +80,7 @@ function MyDeskPage({
 
 export const query = graphql`
   {
-    heroData: file(name: { eq: "my-desk" }) {
+    heroData: file(name: { eq: "my-desk-trim" }) {
       childImageSharp {
         fluid {
           ...GatsbyImageSharpFluid
