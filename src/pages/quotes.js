001ebec6655f4eff8877092c9cbd9f70b84c9079
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-04-23 22:20:09 +0300

    Update quotes.js

--- src/pages/quotes.js
+++ src/pages/quotes.js
@@ -30,9 +30,12 @@ function QuotesPage({ location, data: { quotesData } }) {
             <ColExtra>
               <ExternalList
                 urls={[
+                  { name: 'Github', url: 'https://github.com/ademilter' },
+                  { name: 'Twitter', url: 'https://twitter.com/ademilter' },
+                  { name: 'Medium', url: 'https://medium.com/@ademilter' },
+                  { name: 'YouTube', url: 'https://youtube.com/ademilter' },
                   { name: 'VSCO', url: 'https://vsco.co/adem/gallery' },
-                  { name: 'Instagram', url: 'https://instagram.com/ademilter' },
-                  { name: 'Twitter', url: 'https://twitter.com/ademilter' }
+                  { name: 'Instagram', url: 'https://instagram.com/ademilter' }
                 ]}
               />
             </ColExtra>
