b225c7acc6ccaba5bff5873447daa6c4b5eaf1bd
Berat Bozkurt <beratbozkurt1999@gmail.com>
2020-04-23 22:19:54 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -48,7 +48,9 @@ function IndexPage({ location, data }) {
             <ColExtra>
               <ExternalList
                 urls={[
+                  { name: 'Github', url: 'https://github.com/ademilter' },
                   { name: 'Twitter', url: 'https://twitter.com/ademilter' },
+                  { name: 'Medium', url: 'https://medium.com/@ademilter' },
                   { name: 'YouTube', url: 'https://youtube.com/ademilter' },
                   { name: 'VSCO', url: 'https://vsco.co/adem/gallery' },
                   { name: 'Instagram', url: 'https://instagram.com/ademilter' }
