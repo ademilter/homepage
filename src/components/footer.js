7cec2b1d88636e428d8ab7a70dca434f19b2e7e2
Adem ilter <ademilter@gmail.com>
2020-04-26 13:25:34 +0300

    footer eklendi

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,12 +1,31 @@
 import React from 'react'
 
+import Grid, { ColContent } from './grid'
+import Html from './html'
+
 import styles from './footer.module.css'
 
 function Footer() {
   return (
     <footer className={styles.footer}>
-      <div className="grid">
-        <div></div>
+      <div className="container">
+        <Grid>
+          <ColContent>
+            <Html>
+              <p>
+                Bu web sitesini GatsbyJS ile kodlayıp,{' '}
+                <a
+                  href="https://github.com/ademilter/homepage"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                >
+                  kaynak kodlarını
+                </a>{' '}
+                githab üzerinden paylaştım &lt;3
+              </p>
+            </Html>
+          </ColContent>
+        </Grid>
       </div>
     </footer>
   )
