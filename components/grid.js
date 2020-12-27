9f111f851bc1f5c7e96ffd5ed6f22f27b160a252
Adem ilter <ademilter@gmail.com>
2020-12-27 12:44:17 +0300

    grid refactor

--- components/grid.js
+++ components/grid.js
@@ -1,34 +1,60 @@
 import React from 'react'
+import cn from 'classnames'
 
-export default function Grid({ children }) {
+import styles from './grid.module.css'
+
+export default function Grid({ children, className, props }) {
   return (
-    <r-grid columns="2" columns-t="4" columns-d="12">
+    <div
+      col="2"
+      col-t="4"
+      col-d="12"
+      className={cn(styles.grid, className)}
+      {...props}
+    >
       {children}
-    </r-grid>
+    </div>
   )
 }
 
-export function ColSidebar({ title, children }) {
+export function ColSidebar({ children, className, props }) {
   return (
-    <r-cell span="2" span-t="1" span-d="1-2">
-      {title}
+    <div
+      span="2"
+      span-t="1"
+      span-d="1-2"
+      className={cn(styles.col, className)}
+      {...props}
+    >
       {children}
-    </r-cell>
+    </div>
   )
 }
 
-export function ColContent({ children }) {
+export function ColContent({ children, className, props }) {
   return (
-    <r-cell span="2" span-t="2-4" span-d="3-9">
+    <div
+      span="2"
+      span-t="2-4"
+      span-d="3-10"
+      className={cn(styles.col, className)}
+      {...props}
+    >
       {children}
-    </r-cell>
+    </div>
   )
 }
 
-export function ColExtra({ children }) {
+export function ColExtra({ children, className, props }) {
   return (
-    <r-cell span="2" span-t="2-4" span-d="10-12">
+    <div
+      span="2"
+      span-t="2-4"
+      span-d="11-12"
+      className={cn(styles.col, className)}
+      {...props}
+    >
       {children}
-    </r-cell>
+    </div>
   )
 }
