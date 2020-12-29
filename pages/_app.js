a1171a292f3369b4ddcd2ed109b44e306f0e28c7
Adem ilter <ademilter@gmail.com>
2020-12-29 11:58:09 +0300

    mobile fix

--- pages/_app.js
+++ pages/_app.js
@@ -6,7 +6,7 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <meta name="viewport" content="viewport-fit=cover" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
       <Component {...pageProps} />
     </>
