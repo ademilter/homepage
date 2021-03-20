c8acc495858fe8daaac2b97e780fd626004bdc61
Adem ilter <ademilter@gmail.com>
2021-03-20 16:52:51 +0300

    fix: react to preact

--- next.config.js
+++ next.config.js
@@ -14,6 +14,18 @@ const nextConfig = (phase) => {
     pwa: {
       dest: 'public',
       disable: isDev
+    },
+    webpack: (config, { dev, isServer }) => {
+      // Replace React with Preact only in client production build
+      if (!dev && !isServer) {
+        Object.assign(config.resolve.alias, {
+          react: 'preact/compat',
+          'react-dom/test-utils': 'preact/test-utils',
+          'react-dom': 'preact/compat'
+        })
+      }
+
+      return config
     }
   }
 }
