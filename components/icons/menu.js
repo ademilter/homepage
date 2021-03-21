fe903107dddf80e96c55e37b946f791c7a230838
Adem ilter <ademilter@gmail.com>
2021-03-21 23:19:31 +0300

    refactor: social-card

--- components/icons/menu.js
+++ components/icons/menu.js
@@ -1,10 +1,10 @@
-function IconMenu() {
+function IconMenu({ size = 28 }) {
   return (
     <svg
       xmlns="http://www.w3.org/2000/svg"
       fill="none"
-      width={28}
-      height={28}
+      width={size}
+      height={size}
       viewBox="0 0 24 24"
       stroke="currentColor"
     >
