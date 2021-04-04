55f94c5a1f7d64c41ecd26a5dd36d352f0afc3ff
Adem ilter <ademilter@gmail.com>
2021-04-04 21:19:33 +0300

    delete roomservice

--- pages/_app.js
+++ pages/_app.js
@@ -1,47 +1,11 @@
 import '@style/globals.css'
 
-import { RoomServiceProvider } from '@roomservice/react'
-import useUserID from '../hooks/use-user-id'
 import Head from 'next/head'
 import Header from '@comp/header'
 import Footer from '@comp/footer'
 import { Auth0Provider } from '@auth0/auth0-react'
-import Cat from '@comp/live'
-
-async function myAuthFunction({ room, ctx }) {
-  const response = await fetch('/api/roomservice', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    //  Pass cookies to server
-    credentials: 'include',
-    body: JSON.stringify({
-      room: room,
-      //  TODO: Determine userID on server based on cookies or values passed in here.
-      user: ctx.userID
-    })
-  })
-
-  if (response.status === 401) {
-    throw new Error('Unauthorized!')
-  }
-
-  if (response.status !== 200) {
-    throw await response.text()
-  }
-
-  const body = await response.json()
-  return {
-    user: body.user,
-    resources: body.resources,
-    token: body.token
-  }
-}
 
 export default function MyApp({ Component, pageProps }) {
-  const userID = useUserID()
-
   return (
     <Auth0Provider
       clientId={process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID}
@@ -53,17 +17,11 @@ export default function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
 
-      <RoomServiceProvider
-        online={userID !== null}
-        clientParameters={{ auth: myAuthFunction, ctx: { userID } }}
-      >
-        <Header />
-        <main className="py-14">
-          <Component {...pageProps} />
-        </main>
-        <Footer />
-        <Cat />
-      </RoomServiceProvider>
+      <Header />
+      <main className="py-14">
+        <Component {...pageProps} />
+      </main>
+      <Footer />
     </Auth0Provider>
   )
 }
