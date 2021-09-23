6f44da594acc9c91a810819c5e9667d5a74508f5
Adem ilter <ademilter@gmail.com>
2021-09-23 10:43:06 +0300

    fix

--- pages/moodboard.tsx
+++ pages/moodboard.tsx
@@ -2,9 +2,39 @@ import PageTransition from 'components/page-transition';
 import PageTitle from 'components/page-title';
 import { Dropmark } from 'types/dropmark';
 import Head from 'next/head';
-import { getDropmark } from '../lib/dropmark';
+import useSWR from 'swr';
 
-function MoodboardPage({ items }) {
+function Moodboard() {
+  const { data, error, isValidating } = useSWR('/api/moodboard', {
+    revalidateOnFocus: false,
+  });
+
+  if (isValidating) {
+    return <div>Loading</div>;
+  }
+
+  if (!isValidating && error) {
+    return <div>{error}</div>;
+  }
+
+  return (
+    <div className="grid sm:grid-cols-2 gap-8">
+      {data.map((item: Dropmark) => {
+        return (
+          <div key={item.id} className="mb-8">
+            <img
+              src={item.content}
+              width={item.metadata.width}
+              height={item.metadata.height}
+            />
+          </div>
+        );
+      })}
+    </div>
+  );
+}
+
+function MoodboardPage() {
   return (
     <PageTransition>
       <Head>
@@ -20,33 +50,21 @@ function MoodboardPage({ items }) {
       </div>
 
       <div className="c-large mt-20">
-        <div className="grid sm:grid-cols-2 gap-8">
-          {items.map((item: Dropmark) => {
-            return (
-              <div key={item.id} className="mb-8">
-                <img
-                  src={item.content}
-                  width={item.metadata.width}
-                  height={item.metadata.height}
-                />
-              </div>
-            );
-          })}
-        </div>
+        <Moodboard />
       </div>
     </PageTransition>
   );
 }
 
-export async function getStaticProps() {
-  const items: [Dropmark] = await getDropmark();
-
-  return {
-    props: {
-      items,
-    },
-    // revalidate: 7200,
-  };
-}
+// export async function getStaticProps() {
+//   const items: [Dropmark] = await getDropmark();
+//
+//   return {
+//     props: {
+//       items,
+//     },
+//     // revalidate: 7200,
+//   };
+// }
 
 export default MoodboardPage;
