1f81a64bf86bae5b98d62a13217530c8a3794638
Adem ilter <ademilter@gmail.com>
2022-03-14 23:33:16 +0300

    fix ui

--- components/video-row.tsx
+++ components/video-row.tsx
@@ -1,13 +1,19 @@
 import useLocalStorage from "../hooks/useLocalStorage";
 import slugify from "@sindresorhus/slugify";
-import IconPlay from "./icons/play";
+import cx from "classnames";
+// import IconPlay from "./icons/play";
 
 export default function VideoRow({ title, url, itemsCount }) {
   const [check, setCheck] = useLocalStorage(slugify(title), false);
 
   return (
-    <div className="group flex items-center gap-3 py-3 transition dark:text-white hover:text-red-600 dark:hover:text-red-400">
-      <label className="flex items-center justify-center w-5 h-5 border border-gray-300 rounded shadow-sm text-gray-900 dark:border-gray-600 dark:text-gray-200">
+    <div
+      className={cx(
+        "group flex items-center gap-3 py-3 transition dark:text-white hover:text-red-600 dark:hover:text-red-400",
+        check ? "opacity-50" : ""
+      )}
+    >
+      <label className="flex items-center justify-center w-5 h-5 border border-gray-300 rounded shadow-sm text-gray-900 dark:border-gray-600 dark:text-gray-200 cursor-pointer">
         <input
           className="absolute opacity-0 pointer-events-none"
           type="checkbox"
@@ -20,7 +26,7 @@ export default function VideoRow({ title, url, itemsCount }) {
       </label>
 
       <a href={url} className="flex-grow">
-        <span className="font-semibold">{title}</span>
+        <span className={cx("font-semibold")}>{title}</span>
       </a>
 
       {itemsCount > -1 && (
