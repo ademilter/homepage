f8795054aff0b1fc9a8bad8c4deca2034ff246e0
Adem ilter <ademilter@gmail.com>
2022-03-17 12:31:15 +0300

    fix error

--- pages/api/bookmark/vote/[id].ts
+++ pages/api/bookmark/vote/[id].ts
@@ -8,28 +8,34 @@ export default async function handler(
   const method = req.method;
   const { id } = req.query;
 
-  if (method === "GET") {
-    const count = await redis.get(`bookmark:${id}`);
-    return res.status(200).json({ count });
-  }
-
-  if (method === "PATCH") {
-    const ip =
-      req.headers["x-real-ip"] ||
-      req.headers["x-forwarded-for"] ||
-      req.headers["Remote_Addr"] ||
-      "NA";
-
-    const response =
-      ip === "NA" ? 1 : await redis.sadd(`bookmark:${id.toString()}`, ip);
-
-    if (response === 0) {
-      return res.status(200).json({ message: "Already voted" });
+  try {
+    if (method === "GET") {
+      const count = await redis.get(`bookmark:${id}`);
+      return res.status(200).json({ count });
     }
 
-    const count = await redis.incr(`bookmark:${id}`);
-    return res.status(200).json({ count });
-  }
+    if (method === "PATCH") {
+      const ip =
+        req.headers["x-real-ip"] ||
+        req.headers["x-forwarded-for"] ||
+        req.headers["Remote_Addr"] ||
+        "NA";
 
-  return res.status(400).json({ message: "Method not allowed" });
+      const response =
+        ip === "NA" ? 1 : await redis.sadd(`bookmark:${id.toString()}`, ip);
+
+      if (response === 0) {
+        return res.status(200).json({ message: "Already voted" });
+      }
+
+      const count = await redis.incr(`bookmark:${id}`);
+      return res.status(200).json({ count });
+    }
+
+    return res.status(400).json({ message: "Method not allowed" });
+  } catch (e) {
+    res.status(500).json({
+      error: e.message,
+    });
+  }
 }
