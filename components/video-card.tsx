763e2e7132c9e754c940cad1a367616431772aee
Adem ilter <ademilter@gmail.com>
2022-07-07 00:39:25 +0300

    fix video card

--- components/video-card.tsx
+++ components/video-card.tsx
@@ -1,7 +1,6 @@
 import useLocalStorage from "../hooks/useLocalStorage";
 import slugify from "@sindresorhus/slugify";
 import cx from "classnames";
-// import IconPlay from "./icons/play";
 
 export default function VideoCard({ title, url, itemsCount }) {
   const [check, setCheck] = useLocalStorage(slugify(title), false);
@@ -9,11 +8,11 @@ export default function VideoCard({ title, url, itemsCount }) {
   return (
     <div
       className={cx(
-        "group flex items-center gap-3 py-3 transition hover:text-red-600 dark:text-white dark:hover:text-red-400",
+        "group flex items-start gap-3 py-3 transition hover:text-red-600 dark:text-white dark:hover:text-red-400",
         check ? "opacity-50" : ""
       )}
     >
-      <label className="flex h-5 w-5 cursor-pointer items-center justify-center rounded border border-zinc-300 text-zinc-900 shadow-sm dark:border-zinc-600 dark:text-zinc-200">
+      <label className="mt-0.5 flex h-5 w-5 shrink-0 cursor-pointer items-center justify-center rounded border border-zinc-300 text-zinc-900 shadow-sm dark:border-zinc-600 dark:text-zinc-200">
         <input
           className="pointer-events-none absolute opacity-0"
           type="checkbox"
@@ -30,12 +29,10 @@ export default function VideoCard({ title, url, itemsCount }) {
       </a>
 
       {itemsCount > -1 && (
-        <span className="rounded bg-zinc-100 py-0.5 px-2 text-sm text-zinc-500 dark:bg-zinc-800 dark:text-zinc-500">
-          {`${itemsCount} Videos`}
+        <span className="shrink-0 whitespace-nowrap rounded bg-zinc-100 py-0.5 px-2 text-sm text-zinc-500 dark:bg-zinc-800 dark:text-zinc-500">
+          {`${itemsCount} videos`}
         </span>
       )}
-
-      {/*<IconPlay className="opacity-30 group-hover:opacity-100" />*/}
     </div>
   );
 }
