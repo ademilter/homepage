c353967024374de9aa95a902c22213f04d90621f
Adem ilter <ademilter@gmail.com>
2022-07-13 10:38:00 +0300

    fix tools

--- pages/tools.tsx
+++ pages/tools.tsx
@@ -3,9 +3,18 @@ import PageTitle from "components/page-title";
 import Head from "next/head";
 import { getTable } from "lib/airtables";
 import { Tool } from "types/Tool";
+import { useState } from "react";
+
+const defaultFilter: string = "all";
 
 export default function ToolsPage({ data }) {
   console.log(data);
+  const [category, setCategory] = useState<string>(defaultFilter);
+
+  const categories = [
+    ...new Set(data.flatMap((tool: Tool) => tool.category) as string[]),
+  ];
+
   return (
     <PageTransition>
       <Head>
@@ -16,25 +25,50 @@ export default function ToolsPage({ data }) {
         <PageTitle>
           Uzun süredir kullandığım ve memnun kaldığım uygulamaların listesi.
         </PageTitle>
+        <div className="mt-4 flex items-center gap-2">
+          {["all", ...categories].map((category) => {
+            return (
+              <button
+                key={category}
+                className="rounded border p-2"
+                onClick={() => {
+                  setCategory(category);
+                }}
+              >
+                {category}
+              </button>
+            );
+          })}
+        </div>
       </div>
 
       <div className="c-large mt-20">
         <div className="grid items-end gap-6 sm:grid-cols-2 md:grid-cols-3 md:gap-10 lg:grid-cols-4">
-          {data.map((tool: Tool) => (
-            <div
-              key={tool.Id}
-              className="rounded border border-zinc-100 p-4 dark:border-zinc-800"
-            >
-              <h5 className="opacity-50">{tool.brand}</h5>
-              <h4 className="font-semibold text-white">{tool.name}</h4>
-              {/*<div>{tool.wtf}</div>*/}
-              <div className="flex items-center gap-0.5">
-                <span>{tool.rating}</span>
-                <span className="opacity-50">/</span>
-                <span className="opacity-50">5</span>
-              </div>
-            </div>
-          ))}
+          {data
+            .filter((tool: Tool) => {
+              if (category === "all") return true;
+              return tool.category.includes(category);
+            })
+            .map((tool: Tool) => (
+              <article
+                key={tool.Id}
+                className="rounded border border-zinc-100 p-4 dark:border-zinc-800"
+              >
+                <header>
+                  <h5 className="opacity-50">{tool.brand}</h5>
+                  <h3 className="font-semibold dark:text-white">{tool.name}</h3>
+                </header>
+
+                <div className="mt-4 rounded bg-zinc-100 p-2">
+                  <span className="flex items-center gap-0.5">
+                    <span>{tool.rating}</span>
+                    <span className="opacity-50">/</span>
+                    <span className="opacity-50">5</span>
+                  </span>
+                  <p>{tool.comment}</p>
+                </div>
+              </article>
+            ))}
         </div>
       </div>
     </PageTransition>
--- types/Tool.ts
+++ types/Tool.ts
@@ -1,7 +1,36 @@
+type Images = {
+  filename: string; // "compare_airpods_pro__e9uzt0mzviem_large_2x.png"
+  height: number; // 280;
+  id: string; //"attkoZfaGwKwujdHS"
+  size: number; // 38166;
+  thumbnails: {
+    full: {
+      url: string; // "https://cdn.filestackcontent.com/attkoZfaGwKwujdHS"
+      width: number; // 1024;
+      height: number; // 1024;
+    };
+    large: {
+      url: string; // "https://cdn.filestackcontent.com/attkoZfaGwKwujdHS"
+      width: number; // 1024;
+      height: number; // 1024;
+    };
+    small: {
+      url: string; // "https://cdn.filestackcontent.com/attkoZfaGwKwujdHS"
+      width: number; // 1024;
+      height: number; // 1024;
+    };
+  };
+  type: "image/png";
+  url: string; //"https://dl.airtable.com/.attachments/1878b0b5748da0c79368985c41255d8b/672a0e96/compare_airpods_pro__e9uzt0mzviem_large_2x.png?ts=1657696711&userId=usrfC9OaZ4Sm9kpYZ&cs=e41a8b2b063a49e4"
+  width: number; // 330
+};
+
 export type Tool = {
   Id: string; // "recFoyzP7v567KhqU"
   brand: string; // "Bang&Olufsen"
   category: string[]; // ["workspace", "living"]
+  comment: string; // lorem ipsum
+  images: Images;
   name: string; // "BeoPlay H9"
   rating: 1 | 2 | 3 | 4 | 5; // 4
   url: string; // "https://www.bang-olufsen.com/en/us/headphones/beoplay-h9?variant=beoplay-h9-3-matte-black"
