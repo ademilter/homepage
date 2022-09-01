45c71462db60d786f75f252dd8f749bada7d0573
Adem ilter <ademilter@gmail.com>
2022-09-02 01:45:16 +0300

    remove animation

--- components/photos.tsx
+++ components/photos.tsx
@@ -1,49 +1,38 @@
 import NextImage from "next/image";
 import Link from "@/components/link";
-import { motion } from "framer-motion";
+import cx from "classnames";
 
-function Photo({ links, urls, description, width, height }) {
+function Photo({ alt_description, links, urls, description, width, height }) {
   return (
-    <motion.figure
-      variants={{
-        hidden: { opacity: 0 },
-        visible: { opacity: 1 },
-      }}
-    >
-      <Link href={links.html}>
+    <figure>
+      <Link
+        href={links.html}
+        className={cx(
+          "block overflow-hidden rounded-lg",
+          "saturate-50 transition-all duration-700",
+          "hover:scale-105 hover:saturate-100"
+        )}
+      >
         <NextImage
-          src={urls.regular}
-          alt={description}
+          src={`${urls.raw}&q=90&w=800`}
+          alt={alt_description || description}
           width={width}
           height={height}
           layout="responsive"
           quality={100}
-          className="rounded-lg saturate-50 transition-all duration-700 hover:scale-105 hover:saturate-100"
         />
       </Link>
-    </motion.figure>
+    </figure>
   );
 }
 
 function Photos({ data }) {
   return (
-    <motion.div
-      variants={{
-        visible: {
-          transition: {
-            delayChildren: 0.2,
-            staggerChildren: 0.2,
-          },
-        },
-      }}
-      initial="hidden"
-      animate="visible"
-      className="grid items-end gap-6 sm:grid-cols-2 md:grid-cols-3 md:gap-x-8 md:gap-y-20 lg:grid-cols-4"
-    >
+    <div className="grid items-end gap-6 sm:grid-cols-2 md:grid-cols-3 md:gap-x-8 md:gap-y-20 lg:grid-cols-4">
       {data.map((item) => {
         return <Photo key={item.id} {...item} />;
       })}
-    </motion.div>
+    </div>
   );
 }
 
