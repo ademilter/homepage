8c11b59ba4ae53725cd4ec57cb6732102b352949
Adem ilter <ademilter@gmail.com>
2022-10-11 22:23:56 +0300

    refactor bookmarks atom feed

--- next.config.js
+++ next.config.js
@@ -1,22 +1,13 @@
 const { withContentlayer } = require("next-contentlayer");
 
 /**
- * @type {import("next").NextConfig}
+ * @type {import('next').NextConfig}
  */
 
 module.exports = withContentlayer({
-  swcMinify: true,
   reactStrictMode: true,
   images: {
     domains: ["images.unsplash.com", "dl.airtable.com"],
-  },
-  rewrites() {
-    return Promise.resolve([
-      {
-        source: '/bookmarks/rss.:format(xml|json)',
-        destination: '/api/bookmarks-rss',
-      },
-    ]);
   },
   experimental: {
     legacyBrowsers: false,
