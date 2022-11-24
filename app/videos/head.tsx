4112ab9fef9b86aa63bb50c9fe5cd00d030a0b31
Adem ilter <ademilter@gmail.com>
2022-11-24 07:34:24 +0300

    fix

---
+++ app/videos/head.tsx
@@ -0,0 +1,12 @@
+export default function Head() {
+  return (
+    <>
+      <title>Eğitimler</title>
+      <meta
+        name="description"
+        content="Frontend ve Tasarım alanında ürettiğim eğitim videolarının tam
+          listesi."
+      />
+    </>
+  );
+}
