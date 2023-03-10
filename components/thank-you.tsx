76eb93906aab8d7287d5f07442fdd46f77f5d6bf
Adem ilter <ademilter@gmail.com>
2023-03-10 19:18:04 +0300

    style: buy coffee button

--- components/thank-you.tsx
+++ components/thank-you.tsx
@@ -13,10 +13,10 @@ export default function ThankYou({ className }: ThankYouProps) {
       href="https://www.buymeacoffee.com/ademilter"
       className={cx(
         "flex items-center gap-6 rounded-lg bg-amber-100 px-6 py-5",
-        "text-amber-900 transition-colors",
-        "hover:bg-amber-100 hover:no-underline",
-        "dark:bg-zinc-600 dark:text-zinc-100",
-        "dark:hover:bg-amber-800 dark:hover:text-amber-50",
+        "text-amber-900 transition-all",
+        "hover:bg-amber-200 hover:no-underline",
+        "dark:bg-zinc-700 dark:text-zinc-200",
+        "dark:hover:bg-zinc-600",
         className
       )}
     >
@@ -24,7 +24,7 @@ export default function ThankYou({ className }: ThankYouProps) {
         Ücretsiz yayınladığım içerikler için teşekkür etmek istersen kahve
         ısmarlayabilirsin
       </span>
-      <span className="shrink-0 w-14 h-14 rounded-full bg-white dark:bg-zinc-700 flex items-center justify-center">
+      <span className="shrink-0 w-14 h-14 rounded-full bg-white dark:bg-zinc-600 flex items-center justify-center">
         <Buymeacoffee />
       </span>
     </BaseLink>
