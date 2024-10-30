36f5c2f4bc6751e5f923ad14701407ae2881b70c
Adem ilter <ademilter@gmail.com>
2024-10-31 01:08:50 +0300

    fix

--- src/app/photos/page.tsx
+++ src/app/photos/page.tsx
@@ -1,10 +1,6 @@
-import unsplash from "@/lib/unsplash";
-import MetricCard from "@/components/metric-card";
 import Container from "@/components/container";
 import Photos from "@/components/photos";
-import { SOCIAL } from "@/lib/const";
 import { Metadata } from "next";
-import { formatter } from "@/lib/helper";
 
 export const metadata: Metadata = {
   title: "Fotoğraflar",
@@ -15,28 +11,12 @@ export const metadata: Metadata = {
 export const revalidate = 86400; // 60*60*24
 
 export default async function PhotosPage() {
-  const photos = await unsplash.getPhotos();
-  const stats = await unsplash.getStats();
+  const photos = [] as any;
 
   return (
     <>
       <Container>
         <h1 className="text-xl sm:text-2xl">{metadata.description}</h1>
-
-        <div className="mt-10 grid grid-cols-2 gap-4 sm:gap-6">
-          <MetricCard
-            href={SOCIAL.unsplash}
-            data={formatter.format(stats.views.total)}
-          >
-            Görüntüleme
-          </MetricCard>
-          <MetricCard
-            href={SOCIAL.unsplash}
-            data={formatter.format(stats.downloads.total)}
-          >
-            İndirme
-          </MetricCard>
-        </div>
       </Container>
 
       <Container size="large" className="mt-14 sm:mt-20">
